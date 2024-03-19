ef8b706be8ada3e089ddee174e06089401a320ee
PILSUNyou <dbyo1125@naver.com>
2024-03-19 18:52:33 +0900

    스캐너 입력받기

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,17 +1,21 @@
 package org.example;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        int a = 10;
-        int b = 2;
-        funtion(a,b);
-    }
-    public static int funtion(int a, int b){
-        try {
-            throw new Exception("그냥 에러 띄우기");
-        } catch (Exception e) {
-            throw new RuntimeException(e);
-        }
+        System.out.println("== 프로그램 시작 ==");
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("명령어 입력 : ");
+        String cmd = sc.next();
+        System.out.printf("입력된 명령어 : %s\n", cmd);
+
+        System.out.print("정수 입력 : ");
+        int num = sc.nextInt();
+        System.out.printf("입력된 명령어 : %d\n", num);
+
+        sc.close();
+        System.out.println("== 프로그램 끝 ==");
     }
 }
-
