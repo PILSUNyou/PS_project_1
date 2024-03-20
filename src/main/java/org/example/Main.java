0538a3c1f831c1476b0a8b06447b0b560e5fcd82
PILSUNyou <dbyo1125@naver.com>
2024-03-20 17:43:28 +0900

    게시물 상기능 구현 날짜는 아직

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,29 +1,55 @@
 package org.example;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
+
+import static java.lang.Integer.valueOf;
 
 public class Main {
     public static void main(String[] args) {
         System.out.println("== 프로그램 시작 ==");
         Scanner sc = new Scanner(System.in);
 
+        List<Integer> number = new ArrayList<>();
+        List<Article> articles = new ArrayList<>();
+
         int lastArticleId = 0;
+//        int count = 0;
 
         while (true) {
-            System.out.println("article write : 게시글 쓰기 \narticle list : 게시물 보기 \nexit : 나가기");
+//            if(count == 0) {
+//                PrintSet();
+//                count++;
+//            }
             System.out.print("명령어 입력 : ");
             String cmd = sc.nextLine();
             cmd = cmd.trim();
 
             if (cmd.length()==0){
+                System.out.print("명령어를 입력하세요.");
                 continue;
             }
 
             if (cmd.equals("exit")){
                 break;
             }
+            // 정수 입력하여 숫자 저장하기
+            else if(cmd.equals("num")){
+                System.out.print("정수를 입력 : ");
+                int num = sc.nextInt();
+                sc.nextLine();
 
-            if (cmd.equals("article write")){
+                number.add(num);
+                System.out.println("저장 완료");
+            }
+            // 저장한 정수 출력하기
+            else if (cmd.equals("nums")){
+                System.out.printf("현재 저장된 수는 %s 입니다.\n", number.toString());
+
+            }
+
+            else if (cmd.equals("article write")){
                 int id = lastArticleId + 1;
                 lastArticleId = id;
                 System.out.print("제목 : ");
@@ -31,18 +57,73 @@ public class Main {
                 System.out.print("내용 : ");
                 String body = sc.nextLine();
 
-                System.out.printf("%d번 글이 생성되었습니다.", id);
-
+                Article article = new Article(id, title, body);
+                articles.add(article);
+                System.out.printf("%d번 글이 생성되었습니다.\n", id);
             }
 
             else if (cmd.equals("article list")){
-                System.out.println("게시물이 없습니다.");
+                if(articles.size() == 0) {
+                    System.out.println("게시물이 없습니다.");
+                    continue;
+                }
+                else {
+                    System.out.println("번호 | 제목");
+                    for (int i = 0; i<articles.size(); i++) {
+                        Article article = articles.get(i);
+
+                        System.out.printf("%d    | %s\n", article.id, article.title);
+                    }
+                }
             }
+            else if (cmd.startsWith("article detail ")) {
+
+                String cmdBits = cmd.split(" ")[2];
+                int id = Integer.parseInt(cmdBits);
+                Article foundArticle = null;
+
+                for( int i = 0; i <articles.size();i++){
+                    Article article = articles.get(i);
+
+                    if (article.id == id){
+                        foundArticle = article;
+                        break;
+                    }
+                }
+
+                if (foundArticle == null) {
+                    System.out.printf("%s번 게시물은 존재하지 않습니다.\n", id);
+                    continue;
+                }
+                System.out.printf("번호: %s\n날짜 : %s\n제목 : %s\n내용 : %s\n", foundArticle.id,"2024-03-20 12:12:12", foundArticle.title, foundArticle.body);
+            }
+
             else {
                 System.out.printf("%s(은)는 존재하지 않는 명령어 입니다.\n", cmd);
             }
         }
         sc.close();
         System.out.println("== 프로그램 끝 ==");
+    }
+//    public static void PrintSet(){
+//        System.out.println("=== 입력 가능한 명령어 ===");
+//        System.out.println("num -> 숫자 입력");
+//        System.out.println("nums -> 숫자 확인");
+//        System.out.println("article write -> 게시글 쓰기");
+//        System.out.println("article list -> 게시물 보기");
+//        System.out.println("exit -> 나가기");
+//        System.out.println("=== 입력 가능한 명령어 ===");
+//    }
+}
+
+class Article {
+    int id;
+    String title;
+    String body;
+
+    public Article(int id, String title, String body) {
+        this.id = id;
+        this.title = title;
+        this.body = body;
     }
 }
