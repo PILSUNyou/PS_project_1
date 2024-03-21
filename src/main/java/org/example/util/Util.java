bf4302f06b8375a20d5c6298718c8bb08c3a74c5
PILSUNyou <dbyo1125@naver.com>
2024-03-21 15:58:10 +0900

    코드 리팩토링, 클래스 패키징 분ë리, App클래스로 핵심 ì로직 이전

--- src/main/java/org/example/util/Util.java
+++ src/main/java/org/example/util/Util.java
@@ -1,9 +1,9 @@
-package org.example;
+package org.example.util;
 
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
-public class util {
+public class Util {
     public static  String getNowDateStr(){
         Date now = new Date();
         SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd HH-mm-ss");
